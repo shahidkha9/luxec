c25c9231841af989b1048d667dabde678349b3ac
Muhammad Shahid <shahidkha993@gmail.com>
2025-02-05 10:20:33 +0500

    Added robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://luxec-187i.vercel.app/sitemap.xml",
+  };
+}
